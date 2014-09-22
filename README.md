f30c7dfe7efccb97b9f91a9e52dd7ba7f4eb5192
rammohan2419 <rammohan2419@gmail.com>
2014-09-22 23:27:23 +0530

    Adding the hello world project.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 kick-start
 ==========
 
-This repository contains ruby on rails kick start projects for various requirements/gems.
+This repository contains ruby on rails kick start project for development in rails framework.
 
 
 Here is the wiki to get help with this repository:
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsHelloWorld::Application.config.secret_token = '433628b72287c3edbb9891b0bfd5ecd1796e6a939bde2fbb7c34aa6fe13fead4d2e9b7b7db735981cccba8f9c05fd3ad5b8bcad190dec13cc1222a66ef99ebe4'
